7afe6b215f7a27f4341d4ca18974343aacaa324a
Austin Story <lonnieastory@gmail.com>
2021-02-05 08:25:12 -0600

    Update links to move to show instead of index path.  Root to rake_tasks controller and check for 'load_server' on Rails.application sot hat we can run in development mode to hit the actual Dummy server

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RakeUi::Engine.routes.draw do
+  root to: 'rake_tasks#index'
+
   resources :rake_tasks, only: [:index, :show]
 
   post "/rake_tasks/:id/execute", to: "rake_tasks#execute", as: "rake_task_execute"
