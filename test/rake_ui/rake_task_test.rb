b0b796fbd5e19f4a30fd90d461d8f04803236e2b
Austin Story <lonnieastory@gmail.com>
2021-02-12 06:54:33 -0600

    Update specificity of tests to not be a personal home directory

--- test/rake_ui/rake_task_test.rb
+++ test/rake_ui/rake_task_test.rb
@@ -27,7 +27,7 @@ class RakeTaskTest < ActiveSupport::TestCase
   test "scrubs rake_definition_file to be html safe" do
     task = RakeUi::RakeTask.internal.first
 
-    assert_equal task.rake_definition_file, "/home/talos/projects/dox/rake-ui/test/dummy/lib/tasks/double_nested_tasks.rake:6"
+    assert_includes task.rake_definition_file, "/test/dummy/lib/tasks/double_nested_tasks.rake:6"
   end
 
   test "finds a task by id" do
