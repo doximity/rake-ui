1cf21b9ee79ba093107b2ef83b22a9311a8ab6a5
Austin Story <lonnieastory@gmail.com>
2021-02-12 07:09:46 -0600

    Remove local path desciptions in test files

--- test/dummy/lib/tasks/double_nested_tasks.rake
+++ test/dummy/lib/tasks/double_nested_tasks.rake
@@ -8,5 +8,13 @@ namespace :double_nested do
       puts args
       puts "double_nested_task end"
     end
+    namespace :something_esle do
+      desc "Something specific and stuff"
+      task :double_nested_taskdouble_nested_taskdouble_nested_task, [:user_id, :foo_id] => [:environment] do |_t, args|
+        puts "double_nested_task start"
+        puts args
+        puts "double_nested_task end"
+      end
+    end
   end
 end
--- test/rake_ui/rake_task_log_test.rb
+++ test/rake_ui/rake_task_log_test.rb
@@ -12,17 +12,9 @@ class RakeTaskLogTest < ActiveSupport::TestCase
     assert_equal log.args, "1"
     assert_equal log.environment, "FOO=bar BAZ=biz"
     assert_equal log.rake_command, "FOO=bar BAZ=biz rake nested:the_nested_task[1]"
-    assert_equal log.rake_definition_file, "#<Proc:0x00005558b6239128 /home/talos/projects/dox/rake-ui/test/dummy/lib/tasks/nested_tasks.rake:3>"
+    assert_equal log.rake_definition_file, "#<Proc:0x00005558b6239128 /test/dummy/lib/tasks/nested_tasks.rake:3>"
     assert_equal log.log_file_name, "2021-02-07-09-34-04-0600____nested%3Athe_nested_task.txt"
-    assert_equal log.log_file_full_path, "/home/talos/projects/dox/rake-ui/test/dummy/tmp/rake_ui/2021-02-07-09-34-04-0600____nested%3Athe_nested_task.txt"
-
-    # name: nested:the_nested_task
-    # args: 1
-    # environment: FOO=bar BAZ=biz
-    # rake_command: FOO=bar BAZ=biz rake nested:the_nested_task[1]
-    # rake_definition_file #<Proc:0x00005558b6239128 /home/talos/projects/dox/rake-ui/test/dummy/lib/tasks/nested_tasks.rake:3>
-    # log_file_name: 2021-02-07-09-34-04-0600____nested%3Athe_nested_task.txt
-    # log_file_full_path: /home/talos/projects/dox/rake-ui/test/dummy/tmp/rake_ui/2021-02-07-09-34-04-0600____nested%3Athe_nested_task.txt
+    assert_includes log.log_file_full_path, "/test/dummy/tmp/rake_ui/2021-02-07-09-34-04-0600____nested%3Athe_nested_task.txt"
   end
 
   test "is able to decode some date, id and name so that we don't have to read the full contents of file" do
