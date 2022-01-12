11793ae7a9a2830f33e9d3d6dfee720fb93f15fb
Jean Luis Urena <jlurena@teladochealth.com>
2022-01-12 12:05:54 -0500

    [ISSUE-18] - Remove env guards at root controller

--- app/controllers/rake_ui/application_controller.rb
+++ app/controllers/rake_ui/application_controller.rb
@@ -2,12 +2,5 @@
 
 module RakeUi
   class ApplicationController < ActionController::Base
-    before_action :black_hole_production
-
-    private
-
-    def black_hole_production
-      raise ActionController::RoutingError, "Not Found" unless Rails.env.test? || Rails.env.development?
-    end
   end
 end
