3d48062a7878bf8012d2d76b8cfb439e4d6893ce
NewAlexandria <zak@newalexandria.org>
2024-03-03 17:39:47 -0500

    test for staging env acceptability

--- lib/rake-ui.rb
+++ lib/rake-ui.rb
@@ -4,7 +4,10 @@ require "rake-ui/engine"
 
 module RakeUi
   mattr_accessor :allow_production
+  mattr_accessor :allow_staging
+
   self.allow_production = false
+  self.allow_staging = true
 
   def self.configuration
     yield(self) if block_given?
