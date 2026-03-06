7e53f05b1cdf345603b8b11f55c4d87cecf199ab
Patrick McMorran <pat.mcmorran@fcpeuro.com>
2026-03-06 18:08:46 -0500

    Fix oversight on CI

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -2,7 +2,19 @@
 
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "rails/test_unit/railtie"
+# require "active_storage/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+# require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -61,7 +61,6 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
